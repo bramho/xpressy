65e995cc5eb44f7404fd826751362910f2df777e
bramho <bramhoningh16@gmail.com>
2016-11-22 21:43:37 +0100

    Added devise

--- app/controllers/vacancies_controller.rb
+++ app/controllers/vacancies_controller.rb
@@ -1,4 +1,5 @@
 class VacanciesController < ApplicationController
+   before_action :authenticate_user!, except: [:index, :show]
 
    def index
       @vacancies = Vacancy.all.order('created_at DESC')
