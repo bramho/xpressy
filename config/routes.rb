694ea99048d1763afdb2eb83e239317f2e749f96
bramho <bramhoningh16@gmail.com>
2016-11-24 15:02:43 +0100

    Added ability for admin to create new users

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
+  resources :users, except: :create
+  get 'create_user' => 'users#add_new_user', as: :new_user_by_admin
+  post 'create_user' => 'users#create', as: :create_user
 
   resources :vacancies do
      resources :reactions
