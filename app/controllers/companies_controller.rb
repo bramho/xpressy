783fbaf0179f0b05cb00a7b94132ae86a32b2e8c
bramho <bramhoningh16@gmail.com>
2016-11-25 14:23:52 +0100

    Added functionality to prefent more than one company to

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -5,7 +5,12 @@ class CompaniesController < ApplicationController
    end
 
    def new
-      @company = Company.new
+      if (Company.exists?)
+         flash[:notice] = 'There is already a company defined, you can edit this company.'
+         redirect_to companies_path
+      else
+         @company = Company.new
+      end
    end
 
    def create
@@ -19,11 +24,37 @@ class CompaniesController < ApplicationController
    end
 
    def show
-      @company = Company.find(params[:id])
+      @company = find_company
+   end
+
+   def edit
+      @company = find_company
+   end
+
+   def update
+      @company = find_company
+
+      if @company.update(params[:company].permit(:name, :description, :address, :postal_code, :city))
+         redirect_to @company
+      else
+         render 'edit'
+      end
+   end
+
+   def destroy
+      @company = find_company
+      @company.destroy
+
+      flash[:notice] = 'The company has succesfully been destoyed.'
+      redirect_to root_path
    end
 
    private
       def company_params
          params.require(:company).permit(:name, :description, :address, :postal_code, :city)
+      end
+
+      def find_company
+         Company.find(params[:id])
       end
 end
