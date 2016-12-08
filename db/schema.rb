0db66234c5468823758a0049d722eaf188144795
bramho <bramhoningh16@gmail.com>
2016-12-08 14:33:15 +0100

    Added jQuery to the pick_module step

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161207132609) do
+ActiveRecord::Schema.define(version: 20161208091905) do
 
   create_table "companies", force: :cascade do |t|
     t.string   "name"
@@ -97,7 +97,7 @@ ActiveRecord::Schema.define(version: 20161207132609) do
     t.text     "horizontal_positions_array"
     t.text     "vertical_positions_array"
     t.text     "width_size_array"
-    t.text     "hight_size_array"
+    t.text     "height_size_array"
   end
 
   create_table "vacancy_modules", force: :cascade do |t|
