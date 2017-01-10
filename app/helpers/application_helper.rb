5e47a61b9c04f6d840774dbb2e67fb3165a557db
Koen van der Baan <koenvanderbaan@hotmail.com>
2017-01-10 14:01:27 +0100

    Add active tab in nav styling.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,12 @@ module ApplicationHelper
    def active_tab(link_path)
       current_page?(link_path) ? "tab-current" : ""
    end
+
+   def controller?(*controller)
+      controller.include?(params[:controller])
+   end
+
+   def action?(*action)
+    action.include?(params[:action])
+  end
 end
